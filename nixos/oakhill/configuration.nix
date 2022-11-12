2fc0a03857bb3028e169daf353e6138a3698f879
Nicolás Bonada <nico.bonada@gmail.com>
2022-11-12 16:25:07 -0500

    oakhill config

--- nixos/oakhill/configuration.nix
+++ nixos/oakhill/configuration.nix
@@ -52,41 +52,184 @@
     };
   };
 
-  # FIXME: Add the rest of your current configuration
+  boot = {
+    loader = {
+      # Use the systemd-boot EFI boot loader.
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    # kernelPackages = pkgs.linuxPackages_zen;
+    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    kernelModules = [ "nct6775" ]; # from lm_sensors
+    kernelParams = [ "nohibernate" ]; # zfs doesn't support hibernate
+    supportedFilesystems = [ "zfs" ];
+    initrd.supportedFilesystems = ["zfs"]; # boot from zfs
+    extraModulePackages = with config.boot.kernelPackages; [ zfsUnstable ];
+    zfs.enableUnstable = true;
+  };
 
-  # TODO: Set your hostname
-  networking.hostName = "your-hostname";
+  services.udev.extraRules = ''
+    ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
+  ''; # zfs already has its own scheduler. without this my(@Artturin) computer froze for a second when i nix build something.
 
-  # TODO: This is just an example, be sure to use whatever bootloader you prefer
-  boot.loader.systemd-boot.enable = true;
+  hardware.cpu.amd.updateMicrocode = true;
 
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  users.users = {
-    # FIXME: Replace with your username
-    your-username = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "correcthorsebatterystaple";
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+
+  networking = {
+    hostId = "23c95f57"; # for zfs
+    hostName = "oakhill"; # Define your hostname.
+    networkmanager.enable = true;
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Toronto";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_CA.UTF-8";
+    extraLocaleSettings = { LC_COLLATE = "C.UTF-8"; };
+  };
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    enableCtrlAltBackspace = true;
+    videoDrivers = [ "nvidia" ];
+
+    xkbOptions = "terminate:ctrl_alt_bksp,compose:caps";
+    autoRepeatDelay = 250;
+    autoRepeatInterval = 40;
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        rofi
+        i3status
+        python3Packages.py3status
+        i3lock
+        dunst
       ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = [ "wheel" ];
+    };
+  };
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    # jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is
+    # enabled by default, no need to redefine it in your config for now)
+    # media-session.enable = true;
+  };
+
+  hardware.opengl.driSupport32Bit = true;
+
+  hardware.steam-hardware.enable = true;
+  programs.steam.enable = true;
+
+  hardware.logitech.wireless = {
+    enable = true;
+    enableGraphical = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nico = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    file
+    alsa-utils
+    mullvad-vpn
+    psmisc
+    usbutils
+    sshfs
+    lm_sensors
+    smartmontools
+    arandr
+  ];
+
+  environment.variables.EDITOR = "nvim";
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  services.locate = {
+    enable = true;
+    locate = pkgs.plocate;
+    localuser = null;
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      userServices = true;
     };
   };
 
-  # This sets up a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
+  services.smartd = {
     enable = true;
-    # Forbid root login through SSH.
-    permitRootLogin = "no";
-    # Use keys only. Remove if you want to SSH using password (not recommended)
-    passwordAuthentication = false;
+    notifications.x11.enable = true;
   };
 
+  services.tlp.enable = true;
+
+  services.mullvad-vpn.enable = true;
+
+  services.journald.extraConfig = "SystemMaxUse=500M";
+
+  services.zfs.autoScrub.enable = true;
+  services.zfs.trim.enable = true;
+  services.fstrim.enable = true;
+
+  services.upower.enable = true;
+  services.udisks2.enable = true;
+
+  programs.fish.enable = true;
+
+  programs.gamemode.enable = true;
+  programs.gamemode.settings.general.inhibit_screensaver = 0;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 6600 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "22.05";
+  system.stateVersion = "21.05";
 }
