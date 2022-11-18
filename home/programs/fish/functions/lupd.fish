171923e0f2ebca6f4b3153051cbe73c9950fb57d
Nicolás Bonada <nico.bonada@gmail.com>
2022-11-17 20:40:11 -0500

    [fish] update lupd function to use the flake.lock file

--- home/programs/fish/functions/lupd.fish
+++ home/programs/fish/functions/lupd.fish
@@ -1,6 +1,8 @@
-function lupd --description "Last nix=channel update"
-    set channel "/nix/var/nix/profiles/per-user/root/channels"
-    set last_update (stat -c '%y' $channel | awk '{print $1}')
+function lupd --argument-names 'lockfile' --description "Last flake.lock update"
+    if test -z "$lockfile"
+        set lockfile ~/nix-config/flake.lock
+    end
+    set last_update (stat -c '%y' $lockfile | awk '{print $1}')
     echo "Last update --> "$last_update
     echo "Today       --> "(date +%F)
 end
