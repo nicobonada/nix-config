a703e0ad087e523e8edca30612494483ba91ab3f
Nicolás Bonada <nico.bonada@gmail.com>
2023-05-12 11:32:44 -0400

    fix nupd.fish, run hostname command in test

--- home/programs/fish/functions/nupd.fish
+++ home/programs/fish/functions/nupd.fish
@@ -1,6 +1,6 @@
 function nupd --description 'update system and home'
     nix flake update ~/nix-config
-    if test $HOSTNAME != 'ost-nobonada'
+    if test (hostname) != 'ost-nobonada'
         sudo nixos-rebuild switch --flake ~/nix-config
     end
     home-manager switch --flake ~/nix-config
