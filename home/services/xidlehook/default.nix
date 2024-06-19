c07ffe292c327362afa6d60dafbf3b8ca7572e40
Nicolás Bonada <nico.bonada@gmail.com>
2024-06-19 14:44:59 -0400

    [xidlehook] add i3lock timer, adjust notify-send timer

--- home/services/xidlehook/default.nix
+++ home/services/xidlehook/default.nix
@@ -6,8 +6,12 @@
     not-when-fullscreen = true;
     timers = [
       {
+        delay = 480;
+        command = "${pkgs.libnotify}/bin/notify-send -t 60000 'Idle' 'Locking in 1 minute\nSleeping in 2 minutes'";
+      }
+      {
         delay = 540;
-        command = "${pkgs.libnotify}/bin/notify-send -t 60000 'Idle' 'Sleeping in 1 minute'";
+        command = "i3lock";
       }
       {
         delay = 600;
