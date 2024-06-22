f4f5b579638d109e03159ad7ca8982f51f204207
Nicolás Bonada <nico.bonada@gmail.com>
2024-06-22 15:50:31 -0400

    remove obsolete comment

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -26,10 +26,6 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is
-    # enabled by default, no need to redefine it in your config for now)
-    # media-session.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
