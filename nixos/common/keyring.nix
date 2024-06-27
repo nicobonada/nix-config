579d402114f7af9347140c0055f0adf7e46dcc5f
Nicolás Bonada <nico.bonada@gmail.com>
2024-06-26 21:09:48 -0400

    use gnome-keyring with greetd instead of lightdm

--- nixos/common/keyring.nix
+++ nixos/common/keyring.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.lightdm.enableGnomeKeyring = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
   programs.seahorse.enable = true;
 
   environment.systemPackages = with pkgs; [
