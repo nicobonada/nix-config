7e217e06d575bfc02a9e440324337a6e9ede3268
Nicolás Bonada <nico.bonada@gmail.com>
2024-07-15 12:41:25 -0400

    [mpd] move config to separate file

--- home/services/mpd/default.nix
+++ home/services/mpd/default.nix
@@ -2,17 +2,6 @@
   services.mpd = {
     enable = true;
     network.listenAddress = "any";
-    extraConfig = ''
-      zeroconf_enabled "yes"
-
-      audio_output {
-        type "pipewire"
-        name "Pipewire Sound Server"
-      }
-
-      replaygain "album"
-      replaygain_preamp "0"
-      replaygain_missing_preamp "-6"
-    '';
+    extraConfig = builtins.readFile ./mpd.conf;
   };
 }
