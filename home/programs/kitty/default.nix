9913cac916fd4e91784a6655bd3930cb6a86dac3
Nicolás Bonada <nico.bonada@gmail.com>
2024-07-24 12:12:09 -0400

    [kitty] don't hang when exiting if there are background processes running

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -11,6 +11,9 @@
       enable_audio_bell = "no";
       tab_bar_style = "powerline";
       cursor_blink_interval = 0;
+
+      # Close the window when the child process (usually the shell) exits
+      close_on_child_death = "yes";
     };
     extraConfig = builtins.readFile ./themes/tokyonight_storm.conf;
   };
