2fb8f8f652eec97ef6bee0da13bd8d1aaaa635ab
Nicolás Bonada <nico.bonada@gmail.com>
2024-11-11 08:53:02 -0500

    pipewire camera fix no longer needed

--- nixos/common/sound.nix
+++ nixos/common/sound.nix
@@ -9,19 +9,6 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
-
-    # without the following lines pipewire keeps the camera open, drawing
-    # significant amounts of power on laptops
-    # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/2669#note_2362342
-    wireplumber = {
-      extraConfig = {
-        "10-disable-camera" = {
-          "wireplumber.profiles" = {
-            main."monitor.libcamera" = "disabled";
-          };
-        };
-      };
-    };
   };
 
   environment.systemPackages = with pkgs; [
