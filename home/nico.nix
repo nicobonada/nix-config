a4acba54b4c78301c1fb3e19d6fb576c056c93ad
Nicolás Bonada <nico.bonada@gmail.com>
2025-03-19 13:28:04 -0400

    added temp jamesdsp overlay to fix breakage with newer pipewire

--- home/nico.nix
+++ home/nico.nix
@@ -19,6 +19,21 @@
   nixpkgs = {
     # You can add overlays here
     overlays = [
+      (
+        final: prev: {
+          # https://github.com/wwmm/easyeffects/commit/38bef46bffdb535e2a70c3332719c557ff577e56
+          jamesdsp = prev.jamesdsp.overrideAttrs {
+            patchPhase = ''
+              substituteInPlace src/audio/pipewire/PwPipelineManager.cpp \
+                --replace-fail "pw_node_add_listener" "pw_proxy_add_object_listener" \
+                --replace-fail "pw_link_add_listener" "pw_proxy_add_object_listener" \
+                --replace-fail "pw_module_add_listener" "pw_proxy_add_object_listener" \
+                --replace-fail "pw_client_add_listener" "pw_proxy_add_object_listener" \
+                --replace-fail "pw_device_add_listener" "pw_proxy_add_object_listener"
+              '';
+          };
+        }
+      )
       # If you want to use overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
 
