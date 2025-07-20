34f869b80f37bc2ac1cc61d17b8ee68bc3e8966f
Nicolás Bonada <nico.bonada@gmail.com>
2025-07-20 02:32:34 -0400

    move nix settings to common/

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ inputs, config, pkgs, lib, ... }:
 {
   imports = [
     ./adb.nix
@@ -13,6 +13,23 @@
     ./sound.nix
     ./yubi.nix
   ];
+
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+    };
+  };
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
