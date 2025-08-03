bf41a1ef44b332a7f2e4670959d220f666388c0f
Nicolás Bonada <nico.bonada@gmail.com>
2025-08-03 16:43:05 -0400

    fish: update tokyonight_storm theme

--- home/programs/fish/tokyonight_storm.fish
+++ home/programs/fish/tokyonight_storm.fish
@@ -1,6 +1,6 @@
 # TokyoNight Color Palette
 set -l foreground c0caf5
-set -l selection 364a82
+set -l selection 2e3c64
 set -l comment 565f89
 set -l red f7768e
 set -l orange ff9e64
@@ -17,6 +17,7 @@ set -g fish_color_keyword $pink
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $foreground
 set -g fish_color_end $orange
+set -g fish_color_option $pink
 set -g fish_color_error $red
 set -g fish_color_param $purple
 set -g fish_color_comment $comment
