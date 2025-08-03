7027b39f19e5316c881c49a56ad934dee93c4c6f
Nicolás Bonada <nico.bonada@gmail.com>
2025-08-03 16:43:05 -0400

    fonts: remove unused, install nerd-fonts symbols

--- home/fonts.nix
+++ home/fonts.nix
@@ -3,12 +3,10 @@
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
-    cascadia-code
     comic-neue
     font-awesome_5
     inter
-    nerd-fonts.fira-code
-    nerd-fonts.jetbrains-mono
+    nerd-fonts.symbols-only
     noto-fonts
     noto-fonts-color-emoji
   ];
