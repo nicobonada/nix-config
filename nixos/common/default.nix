50318750d0bb134ea6a433ce63e4c336cf2da994
Nicolás Bonada <nico.bonada@gmail.com>
2025-08-06 00:35:04 -0400

    fix keyboard being detected as joystick (Royal Kludge R65)

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -118,6 +118,9 @@
   services.udev.extraRules = ''
     # Royal Kludge R87 Pro - prevent joystick classification (it's a keyboard)
     SUBSYSTEM=="input", ATTRS{idVendor}=="342d", ATTRS{idProduct}=="e48e", ENV{ID_INPUT_JOYSTICK}="0"
+
+    # Royal Kludge R65 - prevent joystick classification
+    SUBSYSTEM=="input", ATTRS{idVendor}=="342d", ATTRS{idProduct}=="e508", ENV{ID_INPUT_JOYSTICK}="0"
   '';
 
   # Allow kde connect via home-manager
