4ffa382b12fcb212625b69c9be6c555156af88d9
Nicolás Bonada <nico.bonada@gmail.com>
2026-01-27 09:40:53 -0500

    waybar: use stylix themeing

--- home/programs/waybar/default.nix
+++ home/programs/waybar/default.nix
@@ -2,5 +2,7 @@
 {
   programs.waybar.enable = true;
 
-  xdg.configFile."waybar".source = ./settings;
+  xdg.configFile."waybar/config.jsonc".source = ./settings/config.jsonc;
+  xdg.configFile."waybar/oakhill.jsonc".source = ./settings/oakhill.jsonc;
+  xdg.configFile."waybar/seyruun.jsonc".source = ./settings/seyruun.jsonc;
 }
