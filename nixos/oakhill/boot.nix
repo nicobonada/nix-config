7668ccf85057a9dca266906062b801da478ebbd1
Nicolás Bonada <nico.bonada@gmail.com>
2026-04-27 17:07:10 -0400

    disable zfs.forceImportRoot
    the warning: `boot.zfs.forceImportRoot` is using the default value of `true`. It is highly recommended to
     set it to `false`, the new default from 26.11 on, to reduce the risk of data loss. Alternatively, you can silen
    ce this warning by explicitly setting it to `true`.

--- nixos/oakhill/boot.nix
+++ nixos/oakhill/boot.nix
@@ -17,6 +17,7 @@
 
     supportedFilesystems = [ "zfs" ];
     initrd.supportedFilesystems = ["zfs"]; # boot from zfs
+    zfs.forceImportRoot = false;    # recommended setting starting with 26.11
 
     kernelPackages = pkgs.linuxPackages_zen;
   };
