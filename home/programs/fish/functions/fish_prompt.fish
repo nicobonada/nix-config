67eb1a101c46be4448f42f6d3e02b63aebb7330d
Nicolás Bonada <nico@bonada.ca>
2026-08-09 00:55:26 -0400

    fish: simplify prompt to flake/home/tree + nerd shell markers
    Drop envrc/jj/git place icons; only flake vs home vs elsewhere under /.
    Replace ASCII $/# with Nerd Font dollar/hashtag, keep green/red colors.

--- home/programs/fish/functions/fish_prompt.fish
+++ home/programs/fish/functions/fish_prompt.fish
@@ -1,27 +1,18 @@
 function fish_prompt --description 'Write out the prompt'
-    # One Nerd Font icon: first match wins (most specific → default).
-    #   envrc 󰓦  flake   jj 󰋘  git   home 󰋜  default 󰐆
+    # Place icon (first match): flake   home 󰋜  else tree 󰙅
+    # Shell marker: user  (green)  root  (red)
 
     set -l icon
     set -l color
 
-    if __prompt_path_has .envrc
-        set icon 󰓦
-        set color yellow
-    else if __prompt_path_has flake.nix
+    if __prompt_path_has flake.nix
         set icon 
         set color cyan
-    else if __prompt_path_has .jj
-        set icon 󰋘
-        set color magenta
-    else if __prompt_path_has .git
-        set icon 
-        set color red
     else if test "$PWD" = "$HOME" || string match -q -- "$HOME/*" "$PWD"
         set icon 󰋜
         set color blue
     else
-        set icon 󰐆
+        set icon 󰙅
         set color brblack
     end
 
@@ -32,10 +23,10 @@ function fish_prompt --description 'Write out the prompt'
     switch $USER
         case root
             set_color red
-            echo -n '# '
+            echo -n ' '
         case '*'
             set_color green
-            echo -n '$ '
+            echo -n ' '
     end
     set_color normal
 end
